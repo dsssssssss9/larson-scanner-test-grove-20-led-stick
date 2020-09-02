f788252109a343dd6b854f48ca83a103f2238439
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-02 15:48:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
 let strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
-strip.setBrightness(32)
+strip.setBrightness(128)
 strip.clear()
 strip.show()
 let count = 0
 let diff = 1
-let delay = 100
-let wide = 2
+let delay = 2
+let wide = 3
 basic.forever(function () {
-    if (count == 19) {
+    if (count == 17) {
         diff = -1
-    } else if (count == 0) {
+    } else if (count == 2) {
         diff = 1
     }
     count += diff
-    strip.setPixelColor(count, neopixel.rgb(255, 0, 0))
-    strip.setPixelColor(count - 1, neopixel.rgb(128, 0, 0))
-    strip.setPixelColor(count + 1, neopixel.rgb(128, 0, 0))
+    strip.setPixelColor(count, neopixel.rgb(255, 40, 0))
+    strip.setPixelColor(count - 1, neopixel.rgb(96, 0, 0))
+    strip.setPixelColor(count + 1, neopixel.rgb(96, 0, 0))
+    strip.setPixelColor(count - 2, neopixel.rgb(16, 0, 0))
+    strip.setPixelColor(count + 2, neopixel.rgb(16, 0, 0))
     strip.show()
     basic.pause(delay)
     strip.clear()

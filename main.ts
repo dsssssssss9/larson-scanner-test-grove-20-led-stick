2128495d9e8a66493f5ebcb5fd56fc36e0e0dfca
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-02 15:02:12 +0100

    added dif varible to account for direction

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
 let strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
 strip.setBrightness(32)
 strip.clear()
 strip.show()
+let count = 0
+let diff = 1
 basic.forever(function () {
-	
+    if (count == 20) {
+        diff = -1
+    } else if (count == 0) {
+        diff = 1
+    }
+    count += diff
+    strip.setPixelColor(count, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(count - 1, neopixel.colors(NeoPixelColors.Black))
+    strip.show()
 })

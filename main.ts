f0e716988687fc90e9cb6368f0363749cdb6d862
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-02 15:16:55 +0100

    set variables in startup  + changed 20 to 19

--- main.ts
+++ main.ts
@@ -7,15 +7,18 @@ strip.clear()
 strip.show()
 let count = 0
 let diff = 1
+let delay = 16
+let wide = 3
 basic.forever(function () {
-    if (count == 20) {
+    if (count == 19) {
         diff = -1
     } else if (count == 0) {
         diff = 1
     }
     count += diff
     strip.setPixelColor(count, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(count - 1, neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(count + 1, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(count - wide, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(count + wide, neopixel.colors(NeoPixelColors.Black))
     strip.show()
+    basic.pause(delay)
 })

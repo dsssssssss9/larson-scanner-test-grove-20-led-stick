e21e37ccd9948e69b5c3ca7a241f4a2fb83a5530
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-09-02 15:29:54 +0100

    1st attempt at varying pixel redness

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
 let strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
 strip.setBrightness(32)
 strip.clear()
 strip.show()
 let count = 0
 let diff = 1
-let delay = 16
-let wide = 3
+let delay = 100
+let wide = 2
 basic.forever(function () {
     if (count == 19) {
         diff = -1
@@ -16,9 +13,10 @@ basic.forever(function () {
         diff = 1
     }
     count += diff
-    strip.setPixelColor(count, neopixel.colors(NeoPixelColors.Red))
-    strip.setPixelColor(count - wide, neopixel.colors(NeoPixelColors.Black))
-    strip.setPixelColor(count + wide, neopixel.colors(NeoPixelColors.Black))
+    strip.setPixelColor(count, neopixel.rgb(255, 0, 0))
+    strip.setPixelColor(count - 1, neopixel.rgb(128, 0, 0))
+    strip.setPixelColor(count + 1, neopixel.rgb(128, 0, 0))
     strip.show()
     basic.pause(delay)
+    strip.clear()
 })
